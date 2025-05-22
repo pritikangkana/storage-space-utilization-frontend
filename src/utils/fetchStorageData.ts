7fbde5338d4b53e4bdb35cc3757295f99f3e6783
pritikangkana <pritikangkana12@gmail.com>
2025-05-22 13:24:20 +0530

    Updates

--- src/utils/fetchStorageData.ts
+++
@@ -1,9 +0,0 @@
-const fetchStorageData = async (): Promise<{ totalSpace: number; usedSpace: number }> => {
-    const response = await fetch('http://localhost:3001/api/storage');
-    if (!response.ok) {
-        throw new Error('Failed to fetch storage data');
-    }
-    return response.json();
-};
-
-export default fetchStorageData;
